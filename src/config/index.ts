a38467561359268b745da54bd09a3fbef4c04bce
Atharva <dhenenjay@gmail.com>
2025-11-23 11:11:18 -0800

    Create config index

---
+++ src/config/index.ts
@@ -0,0 +1,4 @@
+// Export all configurations
+export { INTEGRATION_CONFIG } from './integration';
+export { DUCKDB_CONFIG } from '../duckdb/config';
+export { TERRATORCH_CONFIG } from '../terratorch/config';
